4509c1ca9cac7f9827501dbedd8dc984428f5a33
Jordane LAURENT <jordane.laurent_student2021@wilder.school>
2023-01-12 17:22:05 +0100

    gameCard component & animation

--- src/app/card-data.model.ts
+++ src/app/card-data.model.ts
@@ -1,5 +1,5 @@
 export interface CardData {
-  imageId: string;
-  state: 'default' | 'flipped' | 'matched'; // default = card back cover. flipped = card front cover. matched = card is matched and removed from the game.
+  imageId: string; // imageId is the name of the image file without the extension (e.g : 'card-1' or 'card-2') if you stored your images in assets, or it could be the id of the Unsplash image you wanna use.
+  state: 'default' | 'flipped' | 'matched'; // default = card back cover. flipped = card front cover. matched = card is matched and removed from the game. Will be same for angular animation states.
 }
 //  Same as defining a model in backend if you're used to nodeJs for example.
--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {
+  style,
+  transition,
+  trigger,
+  state,
+  animate,
+} from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+import { CardData } from 'src/app/card-data.model';
 
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
-  styleUrls: ['./game-card.component.css']
-})
-export class GameCardComponent {
+  styleUrls: ['./game-card.component.css'],
 
+  // We define the animation trigger here. We can also define the states and transitions here but we’ll do that in the template file.
+  // 'trigger' : definition of an animation. Contains set of states and transitions between them. Defined as a string & can be bound to any UI element.
+  // 'state' : set of properties similar to CSS style properties which should be applied to the element when the state is active.
+  // 'transition': specify the animation that should occur when the state changes. This can include the timing, etc.
+  animations: [
+    trigger('cardFlip', [
+      state(
+        'default',
+        style({
+          transform: 'none',
+        })
+      ),
+      state(
+        'flipped',
+        style({
+          transform: 'rotateY(180deg)',
+        })
+      ),
+      transition('default => flipped', [animate('400ms')]),
+      transition('flipped => default', [animate('200ms')]),
+      transition('* => matched', [animate('400ms')]),
+    ]),
+  ],
+})
+export class GameCardComponent implements OnInit {
+  data: CardData = {
+    imageId: 'pDGNBK9A0sk',
+    state: 'default',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  cardClicked() {
+    if (this.data.state === 'default') {
+      this.data.state = 'flipped';
+    } else {
+      this.data.state = 'default';
+    }
+  }
 }
